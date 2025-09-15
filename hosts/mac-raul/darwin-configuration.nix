b272024423904d9d70be4787a71401e7883b408e
macuguita <raulpripri@gmail.com>
2025-09-15 11:49:01 +0200

    added configs for nix-darwin

---
+++ hosts/mac-raul/darwin-configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, hostname, user, ... }:
+
+{
+  imports = [
+  ];
+
+  environment.systemPackages = with pkgs; [
+    btop
+  ];
+
+  homebrew = {
+    enable = true;
+    user = "raul";
+
+    brews = [
+      "coreutils"
+    ];
+
+    casks = [
+      "prismlauncher"
+      "ghostty"
+      "blockbench"
+      "discord"
+      "qbittorrent"
+    ];
+  };
+
+  system.stateVersion = 5;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.zsh = {
+    enable = true;
+    enableSyntaxHighlighting = true;
+    enableAutosuggestions = true;
+  };
+
+  users.users.raul = {
+    home = "/Users/raul";
+    description = "raul";
+    shell = pkgs.zsh;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
