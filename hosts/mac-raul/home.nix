e61b87e79142b0f75779c7c4a59b7582feb631c5
macuguita <raulpripri@gmail.com>
2025-09-17 12:17:24 +0200

    something

---
+++ hosts/mac-raul/home.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  imports = [
+    ./../../homeManagerModules/default.nix
+  ];
+  home.username = "raul";
+  home.homeDirectory = "/Users/raul";
+
+  myHome = {
+    neovim.enable = true;
+  };
+
+  programs.home-manager.enable = true;
+  home.stateVersion = "24.05";
+}
