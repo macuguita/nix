c23afa26c6a4b4ec65b60ecf7980dd20b0350ac4
macuguita <me@macuguita.com>
2026-05-24 14:36:20 +0200

    enable ly

--- modules/nixos/graphical/greeter.nix
+++ modules/nixos/graphical/greeter.nix
@@ -1,12 +1,4 @@
 { pkgs, config, ... }:
 {
-  services.greetd = {
-    enable = config.macuguita.profiles.graphical.enable;
-
-    settings = {
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
-      };
-    };
-  };
+  services.displayManager.ly.enable = true;
 }
