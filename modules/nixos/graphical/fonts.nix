928afec2d729679c849b44682ac98bfae7389446
macuguita <me@macuguita.com>
2026-08-06 18:39:32 +0200

    update computer and fix little problem with home manager
    update again for pluey

--- modules/nixos/graphical/fonts.nix
+++ modules/nixos/graphical/fonts.nix
@@ -66,12 +66,9 @@
     system.userActivationScripts = {
       copy-fonts-local-share = {
         text = ''
-          rm -rf ~/.local/share/fonts
           mkdir -p ~/.local/share/fonts
-          cp ${pkgs.corefonts}/share/fonts/truetype/* ~/.local/share/fonts/
-          cp ${pkgs.vista-fonts}/share/fonts/truetype/* ~/.local/share/fonts/
-          chmod 544 ~/.local/share/fonts
-          chmod 444 ~/.local/share/fonts/*
+          cp -f ${pkgs.corefonts}/share/fonts/truetype/* ~/.local/share/fonts/
+          cp -f ${pkgs.vista-fonts}/share/fonts/truetype/* ~/.local/share/fonts/
         '';
       };
     };
