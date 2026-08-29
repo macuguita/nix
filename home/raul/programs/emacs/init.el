ddb619c47de7d13f6604c3e6d914013b19dc2ff7
macuguita <me@macuguita.com>
2026-08-29 19:09:32 +0200

    fix package pulling thing

--- home/raul/programs/emacs/init.el
+++ home/raul/programs/emacs/init.el
@@ -4,11 +4,16 @@
 
 ;; Package stuff
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(setq package-archives
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
 (package-initialize)
 
-(unless package-archive-contents
-  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
