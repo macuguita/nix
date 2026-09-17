0b78031528a72ace9d9c744294294c92f3e66d5c
macuguita <me@macuguita.com>
2026-09-17 08:49:27 +0200

    fix intellij jdks install (at least in darwin)

--- home/raul/programs/jetbrains.nix
+++ home/raul/programs/jetbrains.nix
@@ -6,21 +6,42 @@
   ...
 }:
 let
+  inherit (lib.attrsets) mapAttrs' optionalAttrs;
   inherit (lib.lists) optionals;
   inherit (lib.modules) mkIf;
   inherit (lib.strings)
     makeLibraryPath
     optionalString
     ;
+
+  ltsJdks = with pkgs; {
+    "openjdk-8" = jdk8;
+    "openjdk-11" = jdk11;
+    "openjdk-17" = jdk17;
+    "openjdk-21" = jdk21;
+    "openjdk-25" = jdk25;
+  };
+
+  mkJdkLinks =
+    prefix:
+    ltsJdks
+    |> mapAttrs' (
+      name: jdk: {
+        name = "${prefix}/${name}";
+        value = {
+          source = jdk.passthru.home or (jdk.home or jdk);
+        };
+      }
+    );
 in
 {
   config = mkIf osConfig.macuguita.profiles.graphical.enable {
+    home.file =
+      (mkJdkLinks ".jdks") // optionalAttrs pkgs.stdenv.hostPlatform.isDarwin (mkJdkLinks "Library/Java/JavaVirtualMachines");
+
     home.packages =
       let
-        isLinux = pkgs.stdenv.hostPlatform.isLinux;
-        isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
-
-        extraLibs = optionals isLinux (
+        extraLibs = optionals pkgs.stdenv.hostPlatform.isLinux (
           with pkgs;
           [
             libpulseaudio
@@ -42,27 +63,26 @@ in
             "com.demonwav.minecraft-dev"
             "dev.kikugie.stonecutter"
             "GLSL"
-            "IdeaVIM"
           ]).overrideAttrs
-          (old: {
-            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ optionals isLinux [ pkgs.makeWrapper ];
+            (old: {
+              nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ optionals pkgs.stdenv.hostPlatform.isLinux [ pkgs.makeWrapper ];
 
-            # upstream `addPlugins` misses the darwin `open -na` launcher when
-            # rewriting paths, tripping its own disallowedReferences check
-            # (appending to buildPhase since it overrides the stdenv hooks)
-            buildPhase =
-              (old.buildPhase or "")
-              + optionalString isDarwin ''
-                substituteInPlace "$out/bin/idea" --replace-quiet '${old.src}' "$out"
-              '';
+              # upstream `addPlugins` misses the darwin `open -na` launcher when
+              # rewriting paths, tripping its own disallowedReferences check
+              # (appending to buildPhase since it overrides the stdenv hooks)
+              buildPhase =
+                (old.buildPhase or "")
+                + optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+                  substituteInPlace "$out/bin/idea" --replace-quiet '${old.src}' "$out"
+                '';
 
-            postFixup =
-              (old.postFixup or "")
-              + optionalString isLinux ''
-                wrapProgram $out/bin/idea \
-                  --prefix LD_LIBRARY_PATH : "${makeLibraryPath extraLibs}"
-              '';
-          })
+              postFixup =
+                (old.postFixup or "")
+                + optionalString pkgs.stdenv.hostPlatform.isLinux ''
+                  wrapProgram $out/bin/idea \
+                    --prefix LD_LIBRARY_PATH : "${makeLibraryPath extraLibs}"
+                '';
+            })
         )
       ];
   };
